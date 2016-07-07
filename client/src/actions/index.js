e7bdf2727255e5e741bc89745b543d0659d41d57
Mike Ulvila <mikeulvila@me.com>
2016-07-07 16:54:17 -0500

    addTodo action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
 export const GET_TODOS = 'GET_TODOS';
+export const ADD_TODO = 'ADD_TODO';
 
 export function getTodos() {
-  console.log('called getTodos');
   const request = axios.get('/api/todos');
   return {
     type: GET_TODOS,
+    payload: request
+  };
+}
+
+export function addTodo() {
+  const request = axios.post('/api/todos');
+  return {
+    type: ADD_TODO,
     payload: request
   };
 }
