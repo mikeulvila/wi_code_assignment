ec1cbae468841009e8f99b1e8219f5be90cff917
Mike Ulvila <mikeulvila@me.com>
2016-07-06 21:35:10 -0500

    export getTodos action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const GET_TODOS = 'GET_TODOS';
+
+const ROOT_URL = 'http://localhost:3000/api';
+
+export function getTodos() {
+  const request = axios.get(`${ROOT_URL}/todos`);
+  return {
+    type: GET_TODOS,
+    payload: request
+  };
+}
